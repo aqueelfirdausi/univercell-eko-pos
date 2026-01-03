55b3fd078a1ca9163319f2c1af69f879cf8b5ee8
aqueelfirdausi <aqueelfirdausi@gmail.com>
2026-01-03 20:57:15 +0500

    Update login.js

--- js/login.js
+++ js/login.js
@@ -1,9 +1,11 @@
+console.log("LOGIN.JS LOADED");
+
 import { auth } from "./firebase.js";
 import { signInWithEmailAndPassword } from
   "https://www.gstatic.com/firebasejs/10.7.1/firebase-auth.js";
 
+const loginBtn = document.getElementById("loginBtn");
 const message = document.getElementById("message");
-constconst message = document.getElementById("message");
 
 loginBtn.addEventListener("click", async () => {
   const email = document.getElementById("email").value;
