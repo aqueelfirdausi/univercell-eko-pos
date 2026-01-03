adf55b55b18c4a08257a3f98ad696b16bf13f4f7
aqueelfirdausi <aqueelfirdausi@gmail.com>
2026-01-03 19:56:50 +0500

    Create login.js

---
+++ js/login.js
@@ -0,0 +1,18 @@
+import { auth } from "./firebase.js";
+import { signInWithEmailAndPassword } from
+  "https://www.gstatic.com/firebasejs/10.7.1/firebase-auth.js";
+
+const loginBtn = document.getElementById("loginBtn");
+constconst message = document.getElementById("message");
+
+loginBtn.addEventListener("click", async () => {
+  const email = document.getElementById("email").value;
+  const password = document.getElementById("password").value;
+
+  try {
+    await signInWithEmailAndPassword(auth, email, password);
+    message.innerText = "✅ Login successful";
+  } catch (error) {
+    message.innerText = "❌ " + error.message;
+  }
+});
