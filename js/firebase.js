0c0d3b59295e16262405166a8999622889cd8d90
aqueelfirdausi <aqueelfirdausi@gmail.com>
2026-01-05 00:20:00 +0500

    Update firebase.js

--- js/firebase.js
+++ js/firebase.js
@@ -1,24 +1,17 @@
-// Firebase core (browser-safe CDN)
 import { initializeApp } from "https://www.gstatic.com/firebasejs/10.7.1/firebase-app.js";
-
-// Firebase Authentication
 import { getAuth } from "https://www.gstatic.com/firebasejs/10.7.1/firebase-auth.js";
+import { getFirestore } from "https://www.gstatic.com/firebasejs/10.7.1/firebase-firestore.js";
 
-// Firebase configuration (UniverCell-Eko-POS)
 const firebaseConfig = {
-  apiKey: "AIzaSyCeINS_1pf7FF2a5W1DVst71gQg4Zq1KaM",
-  authDomain: "univercell-eko-pos.firebaseapp.com",
-  projectId: "univercell-eko-pos",
-  storageBucket: "univercell-eko-pos.firebasestorage.app",
-  messagingSenderId: "927485837084",
-  appId: "1:927485837084:web:9fe0928147f7893ee4c4d9"
+  apiKey: "YOUR_API_KEY",
+  authDomain: "YOUR_PROJECT.firebaseapp.com",
+  projectId: "YOUR_PROJECT_ID",
+  storageBucket: "YOUR_PROJECT.appspot.com",
+  messagingSenderId: "XXXX",
+  appId: "XXXX"
 };
 
-// Initialize Firebase
 const app = initializeApp(firebaseConfig);
 
-// Initialize Auth
-const auth = getAuth(app);
-
-// Export auth for login.js
-export { auth };
+export const auth = getAuth(app);
+export const db = getFirestore(app);
