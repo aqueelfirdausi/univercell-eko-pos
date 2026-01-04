821243609283b1d5bf01ea44bb3cf39353307155
aqueelfirdausi <aqueelfirdausi@gmail.com>
2026-01-04 23:05:39 +0500

    Update logout.js

--- js/logout.js
+++ js/logout.js
@@ -1,11 +1,12 @@
 import { auth } from "./firebase.js";
-import { signOut } from "https://www.gstatic.com/firebasejs/10.7.1/firebase-auth.js";
+import { signOut } from
+  "https://www.gstatic.com/firebasejs/10.7.1/firebase-auth.js";
 
 const logoutBtn = document.getElementById("logoutBtn");
 
 if (logoutBtn) {
-  logoutBtn.addEventListener("click", async () => {
+  logoutBtn.onclick = async () => {
     await signOut(auth);
     window.location.href = "login.html";
-  });
+  };
 }
