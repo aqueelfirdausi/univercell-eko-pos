85173a4db947b065c7329e209ec47309369f2e66
aqueelfirdausi <aqueelfirdausi@gmail.com>
2026-01-04 23:04:53 +0500

    Update pos-guard.js

--- js/pos-guard.js
+++ js/pos-guard.js
@@ -1,6 +1,7 @@
 import { auth } from "./firebase.js";
 import { onAuthStateChanged } from
   "https://www.gstatic.com/firebasejs/10.7.1/firebase-auth.js";
+
 import { getFirestore, doc, getDoc } from
   "https://www.gstatic.com/firebasejs/10.7.1/firebase-firestore.js";
 
@@ -12,18 +13,9 @@ onAuthStateChanged(auth, async (user) => {
     return;
   }
 
-  const ref = doc(db, "users", user.uid);
-  const snap = await getDoc(ref);
+  const snap = await getDoc(doc(db, "users", user.uid));
 
-  if (!snap.exists()) {
-    window.location.href = "login.html";
-    return;
-  }
-
-  const role = snap.data().role;
-
-  if (role !== "admin" && role !== "staff") {
-    alert("Access denied");
+  if (!snap.exists() || snap.data().role !== "staff") {
     window.location.href = "login.html";
   }
 });
